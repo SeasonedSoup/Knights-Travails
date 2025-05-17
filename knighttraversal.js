c503237721bd6d9bda04ee253c501516579137e4
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-05-17 23:43:41 +0800

    Implement queue, set and directions
    Currently not working since I have not implemented the needed checks to skip a certain direction and as well as counter

--- knighttraversal.js
+++ knighttraversal.js
@@ -1,6 +1,30 @@
 
 function knightMoves(start, end) {
+  if (isOutOfBounds(start) || isOutOfBounds(end)) {
+    console.log("out of bounds");
+    return;
+  }
+  
+  const directions = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+  const queue = [];
+  const visited = new Set();
+  const counter = 0
 
+  queue.push(start)
+  
+  while (queue.length !== 0) {
+    visited.add(queue[0].join(','));
+    for (let i = 0; i < queue.length(); i++) {
+      let cur = queue.shift();
+      if (cur[0] === end[0] && cur[1] === end[1]) {
+        return counter;
+      }
+      for (let [dr, dc] of directions) {
+         let next = [cur[0] + dr, cur[1] + dc]
+      }
+    }
+  }
+  counter++;
 }
 
 function isOutOfbounds(arr) {
