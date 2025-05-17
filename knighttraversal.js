af312f0a1a651d19a6a173bac823f24e045334e4
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-05-17 16:18:04 +0800

    Update knighttraversal.js
    Create out of bounds check  function within 8x8 square

--- knighttraversal.js
+++ knighttraversal.js
@@ -2,3 +2,13 @@
 function knightMoves(start, end) {
 
 }
+
+function isOutOfbounds(arr) {
+  let [dr, dc] = arr;
+
+  if (dr < 0 || dc < 0 || dr > 7 || dc > 7) {
+    return true;
+  } else {
+    return false;
+  }
+}
